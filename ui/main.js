2418a465f59b32388887de45d8a8d544c8e26d9e
Mohiddin Yallur <mohiddin.myallur@yahoo.com>
2016-10-22 02:02:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,4 @@
-/************************************************************************
-  @ManBearPigCode
-  October 13, 2016
 
- Hover Shake causes HTML elements to shake on hover.
-
-  Dependencies: jQuery
-  Important note: elements must display block or inline-block.
-
-  First parameter is a string that represents an HTML element.
-  For example: "#title-header" or ".thoseItems"
-  Second parameter is an integer that represents the time in miliseconds
-  it will take for the element to oscillate to one side. Start with 100
-  for an average speed.
-  Third parameter is an integer representing the number of degrees
-  the element will oscillate. Start with 1 for average shake.
-
-  An example of a call to hoverShake: hoverShake('myImg', 100, 1);
- ************************************************************************/
 
 function hoverShake(myElement, speed, deg) {
 // On mouse enter rotate element on timed interval. For symmetric oscillation,
