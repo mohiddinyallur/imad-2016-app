79800e5b5c73730ef0c684a5cab06df30a25fcf7
Mohiddin Yallur <mohiddin.myallur@yahoo.com>
2016-11-09 22:07:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var config = {
-    user: 'mohiddinmyallur',
-    database: 'mohiddinmyallur',
+    user: 'mohiddimyallur',
+    database: 'mohiddinyallur',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
