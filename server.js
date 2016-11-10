a3834a546775bea67d6da72dcb181f67d8f29710
Mohiddin Yallur <mohiddin.myallur@yahoo.com>
2016-11-10 09:50:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -172,7 +172,7 @@ app.get('/get-articles', function (req, res) {
 app.get('/get-comments/:articleName', function (req, res) {
    // make a select request
    // return a response with the results
-   pool.query('SELECT comment.*, "user".username FROM article, comment, "User_Login" WHERE article.title = $1 AND article.id = comment.article_id AND comment.user_id = "User_Login".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result) {
+   pool.query('SELECT comment.*, "User_Login".username FROM article, comment, "User_Login" WHERE article.title = $1 AND article.id = comment.article_id AND comment.user_id = "User_Login".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
@@ -195,7 +195,7 @@ app.post('/submit-comment/:articleName', function (req, res) {
                     var articleId = result.rows[0].id;
                     // Now insert the right comment for this article
                     pool.query(
-                        "INSERT INTO comment (comment, article_id, user_id) VALUES ($1, $2, $3)",
+                        "INSERT INTO comment (comment, article_id, User_Login_id) VALUES ($1, $2, $3)",
                         [req.body.comment, articleId, req.session.auth.userId],
                         function (err, result) {
                             if (err) {
