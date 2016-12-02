ac6c370f13199e38c011a8386c217fa5dff4bd02
Mohiddin Yallur <mohiddin.myallur@yahoo.com>
2016-12-02 15:13:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/hash/:input', function(req, res) {
    res.send(hashedString);
 });
 
-app.post('/create-user', function (req, res) {
+app.post('/Register', function (req, res) {
   var username = req.body.username;
   var password = req.body.password;
   if(!username.trim() || !password.trim() || username.length>32 || password.length>32){
